ba09383cd7699fe243fedb8029925c9831db92c8
akhil <iamakhilbg@gmail.com>
2022-07-26 09:35:59 +0530

    Config files edited

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  images: {
+    loader: 'akamai',
+    path: ''
+  },
+  basepath: '/nextjs-pages',
+  assetPrefix: 'nextjs-pages'
 }
 
 module.exports = nextConfig
